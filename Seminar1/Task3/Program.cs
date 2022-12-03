4bd8d3a2b1001024ea0a60416a3e9e950e3c65af
Andreeva Nadezhda <andreevanadezdav@gmail.com>
2022-12-04 02:01:35 +0300

    task3

---
+++ Seminar1/Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите целое число:");
+            int number = int.Parse(Console.ReadLine());
+            int count = 1;
+            while (count < number)
+            {
+                if (count % 2 == 0)
+                {
+                Console.Write("Четное число = ");
+                Console.WriteLine(count);
+                count++;
+                }
+                else
+                {
+                count++;
+                }
+            }
